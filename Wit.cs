d2615c0fb073982afc8d187c7e116c467fc9c52f
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2017-01-09 23:56:21 -0300

    Batch commit

--- Wit.cs
+++ Wit.cs
@@ -5,32 +5,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Wit.NET;
 
 namespace MagisterBotApi
 {
     public class Wit
     {
+        private string WIT_API_HOST = "https://api.wit.ai";
+        private string WIT_API_VERSION = "20170107";
+        private int DEFAULT_MAX_STEPS = 5;
+        private string LEARN_MORE = "Learn more at https://wit.ai/docs/quickstart";
+
+
         private RestClient client;
-        private Dictionary<int, int> Sessions;
-        private Dictionary<dynamic, dynamic> Actions;
+        private Dictionary<int, int> sessions;
+        private Dictionary<string, dynamic> actions;
 
-        public Wit(string accessToken)
+        public Wit(string accessToken, Dictionary<string, dynamic> actions = null)
         {
-            client = new RestClient("https://api.wit.ai/");
-            client.AddDefaultHeader("Authorization", string.Format("Bearer {0}", accessToken));
-            client.AddDefaultHeader("Content-Type", "application/json");
-            client.AddDefaultHeader("Accept", "application/json");
-            client.AddDefaultParameter("v", "20170107", ParameterType.QueryString);
-            client.AddDefaultParameter("session_id", "123abc", ParameterType.QueryString);
+            client = PrepareRestClient(accessToken);
 
-            Sessions = new Dictionary<int, int>();
-            Actions = new Dictionary<dynamic, dynamic>();
+            sessions = new Dictionary<int, int>();
+            
+            if (actions != null)
+            {
+                this.actions = ValidateActions(actions);
+            }
+
+            this.actions = new Dictionary<string, dynamic>();
         }
 
-        public MessageResponse Message(MessageRequest message)
+        private RestClient PrepareRestClient(string accessToken)
+        {
+            RestClient restClient = new RestClient(WIT_API_HOST);
+            restClient.AddDefaultHeader("Authorization", string.Format("Bearer {0}", accessToken));
+            restClient.AddDefaultHeader("Content-Type", "application/json");
+            restClient.AddDefaultHeader("Accept", "application/json");
+            restClient.AddDefaultParameter("v", WIT_API_VERSION, ParameterType.QueryString);
+
+            return restClient;
+        }
+
+        private Dictionary<string, dynamic> ValidateActions(Dictionary<string, dynamic> actions)
+        {
+            if (!actions.ContainsKey("send"))
+            {
+                Console.WriteLine("The send action is missing.");
+            }
+
+            return actions;
+        }
+
+        public MessageResponse Message(string msg, bool verbose = true)
         {
             var request = new RestRequest("message", Method.GET);
-            request.AddQueryParameter("q", message.Query);
+            request.AddQueryParameter("q", msg);
 
             IRestResponse responseObject = client.Execute(request);
             MessageResponse response = JsonConvert.DeserializeObject<MessageResponse>(responseObject.Content);
@@ -38,15 +67,15 @@ namespace MagisterBotApi
             return response;
         }
 
-        public ConverseResponse Converse(ConverseRequest converse)
+        public ConverseResponse Converse(int sessionId, string message, Dictionary<string, dynamic> context, bool verbose = true)
         {
-            if (converse.Context == null)
+            if (context == null)
             {
-                converse.Context = new Dictionary<string, dynamic>();
+                context = new Dictionary<string, dynamic>();
             }
 
             var request = new RestRequest("converse", Method.POST);
-            request.AddQueryParameter("q", converse.Message);
+            request.AddQueryParameter("q", message);
 
             IRestResponse responseObject = client.Execute(request);
             ConverseResponse response = JsonConvert.DeserializeObject<ConverseResponse>(responseObject.Content);
@@ -54,56 +83,106 @@ namespace MagisterBotApi
             return response;
         }
 
-        public void RunActions(ConverseRequest message)
+        public Dictionary<string, dynamic> RunActions(int sessionId, string message, Dictionary<string, dynamic> context,
+                                                      int maxSteps = 5, bool verbose = true)
         {
-            if (message.Context == null)
+            if (context == null)
             {
-                message.Context = new Dictionary<string, dynamic>();
+                context = new Dictionary<string, dynamic>();
             }
 
+            /** Figuring out whether we need to reset the last turn.
+                Each new call increments an index for the session.
+                We only care about the last call to run_actions.
+                All the previous ones are discarded (preemptive exit).*/
             int currentRequest = 1;
-            if (Sessions.ContainsKey(message.SessionId))
+            if (sessions.ContainsKey(sessionId))
             {
-                currentRequest = Sessions[message.SessionId] + 1;
+                currentRequest = sessions[sessionId] + 1;
             }
-            Sessions[message.SessionId] = currentRequest;
+            sessions[sessionId] = currentRequest;
+
+            context = _RunActions(sessionId, currentRequest, message, context, maxSteps, verbose);
+
+            // Cleaning up once the last call to run_actions finishes.
+            if (currentRequest == sessions[sessionId])
+            {
+                sessions.Remove(sessionId);
+            }
+
+            return context;
         }
 
 
-        private Dictionary<string, dynamic> RunActions(int sessionId, int currentRequest, string message, Dictionary<string, dynamic> context)
+        private Dictionary<string, dynamic> _RunActions(int sessionId, int currentRequest,
+                                                    string message, Dictionary<string, dynamic> context, int maxSteps = 5, bool verbose = true)
         {
-            ConverseResponse response = Converse(new ConverseRequest(sessionId, message, context));
 
-            if (currentRequest != Sessions[sessionId])
+            if (maxSteps <= 0)
+            {
+                throw new WitException("Max steps reached, stopping.");
+            }
+            ConverseResponse json = Converse(sessionId, message, context);
+            if (json.Type == null)
+            {
+                throw new WitException("Couldn\'t find type in Wit response");
+            }
+
+            if (currentRequest != sessions[sessionId])
             {
                 return context;
             }
+
+
+
+            // backwards-compatibility with API version 20160516
+            if (json.Type == "merge")
+            {
+                json.Type = "action";
+                json.Action = "merge";
+            }
+
+            if (json.Type == "error")
+            {
+                throw new Exception("Oops, I don\'t know what to do.");
+            }
+
+            if (json.Type == "stop")
+            {
+                return context;
+            }
+
 
             ConverseRequest request = new ConverseRequest();
 
             request.SessionId = sessionId;
             request.Context = context;
             request.Message = message;
-            request.Entities = response.Entities;
+            request.Entities = json.Entities;
 
 
-            switch(response.Type)
+            switch (json.Type)
             {
 
                 case "msg":
-                    this.Actions["send"] = response;
+                    Dictionary<string, dynamic> response = new Dictionary<string, dynamic>();
+                    response["text"] = json.Msg;
+                    response["quickreplies"] = json.QuickReplies;
                     break;
                 case "action":
                     string action = response.Action;
-                    context = Actions[action];
+                    context = actions[action];
                     break;
-                case "stop":
-                    return context;
-
+                default:
+                    throw new WitException($"unknown type:  {json.Type}");
             }
 
+            if (currentRequest != sessions[sessionId])
+            {
+                return context;
+            }
 
-            return RunActions(sessionId, currentRequest, null, context);
+            return _RunActions(sessionId, currentRequest, null, context, maxSteps, verbose);
         }
     }
 }
---
+++ WitException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wit.NET
+{
+    class WitException : Exception
+    {
+        public WitException(string message) : base(message) { }
+    }
+}
